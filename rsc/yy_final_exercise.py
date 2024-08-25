832d435dcb9894e7ffb29f2ddf9064d457687ed0
YehYeh <yehyeh@users.noreply.github.com>
2024-08-25 23:52:31 +0300

    fix

--- rsc/yy_final_exercise.py
+++ rsc/yy_final_exercise.py
@@ -12,8 +12,10 @@ def demo(path):
     print(dataset.column_names)
     print(dataset.labels)
     print(dataset)
-    features = [0, 3]
-    dataset.plot_scatter('plot1.png', features)
+
+    two_features = [0, 3]
+    dataset.plot_scatter('plot1.png', two_features)
+
     for i, (observation, label) in enumerate(dataset):
         print(observation, label)
         if i == 3:
@@ -25,7 +27,7 @@ def demo(path):
         try:
             sub_dataset = dataset.filter_by_label(label)
             print(sub_dataset)
-            sub_dataset.plot_scatter(f"plot_{label}.png", features)
+            sub_dataset.plot_scatter(f"plot_{label}.png", two_features)
         except KeyError:
             print(f"no such label")
 
@@ -39,17 +41,17 @@ class Dataset:
 
     def load_from_path(self, path):
         df = pd.read_csv(path)
-
-        self.data = df.iloc[:, 1:].values
-        self.labels = df.iloc[:, 0].values
         self.column_names = df.columns[1:].tolist()
+        # Ignore the label
+        self.data = df.iloc[:, 1:len(self.column_names)].values
+        self.labels = df.iloc[:, -1].values
+        self.num_classes = len(set(self.labels))
 
     def __len__(self):
         return len(self.data)
 
     def __str__(self):
-        num_classes = len(set(self.labels))
-        return f"A dataset object with {len(self.data)} observations, {len(self.column_names)} features and {num_classes} classes"
+        return f"A dataset object with {len(self.data)} observations, {len(self.column_names)} features and {self.num_classes} classes"
 
     def __iter__(self):
         self.index = 0
@@ -63,16 +65,19 @@ class Dataset:
             return observation, label
         raise StopIteration
 
-    def filter_by_label(self, label):
-        if label not in self.labels:
-            raise KeyError(f"Label '{label}' not found in the dataset")
-        indices = np.where(self.labels == label)[0]
+    def filter_by_label(self, value):
+        print(f"filter by label: {value}")
+        indices = np.where(self.labels == value)[0]
+        print(f"filter by label: {indices}")
+        if len(indices) == 0:
+            raise KeyError(f"Label '{value}' not found in the dataset")
         return Dataset(self.data[indices], self.labels[indices], self.column_names)
 
     def plot_scatter(self, path, feature_indices):
         x = self.data[:, feature_indices[0]]
         y = self.data[:, feature_indices[1]]
-        plt.scatter(x, y, c=self.labels)
+        colors = np.random.rand(self.num_classes, 3)
+        plt.scatter(x, y, c=colors)
         plt.xlabel(self.column_names[feature_indices[0]])
         plt.ylabel(self.column_names[feature_indices[1]])
         plt.savefig(path)
